51a90024e493d3b5062114a4abba76dca92c71d2
harshinipechetti <147488891+harshinipechetti@users.noreply.github.com>
2023-12-20 13:34:18 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int last_digit=a[0];     for(i=1;i<n;i++)     {         if(a[i]%2==1)         {             last_digit=i;         }     }     printf("%d",last_digit); }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int last_digit=a[0];
+    for(i=1;i<n;i++)
+    {
+        if(a[i]%2==1)
+        {
+            last_digit=i;
+        }
+    }
+    printf("%d",last_digit);
+}
