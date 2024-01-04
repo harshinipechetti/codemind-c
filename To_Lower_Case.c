a907aa33e994823b4688b2d3fcf1e51517879eb0
harshinipechetti <147488891+harshinipechetti@users.noreply.github.com>
2024-01-04 23:06:31 +0530

    #include <stdio.h> #include <ctype.h> #include <string.h>
    void toLower(char *str) {
        int i;
        for (i = 0; str[i] != NULL; i++) {
            str[i] = tolower(str[i]);
        }
    }
    
    int main() {
        char input[100];
        fgets(input, sizeof(input), stdin);
        if (input[strlen(input) - 1] == '
    ') {
            input[strlen(input) - 1] = NULL;
        }
    
        toLower(input);
        printf("%s
    ", input);
    
        return 0;
    }

---
+++ To_Lower_Case.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+void toLower(char *str) {
+    int i;
+    for (i = 0; str[i] != NULL; i++) {
+        str[i] = tolower(str[i]);
+    }
+}
+
+int main() {
+    char input[100];
+    fgets(input, sizeof(input), stdin);
+    if (input[strlen(input) - 1] == '
+') {
+        input[strlen(input) - 1] = NULL;
+    }
+
+    toLower(input);
+    printf("%s
+", input);
+
+    return 0;
+}
